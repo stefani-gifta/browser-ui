9061eacb17a6b073cdf8f8cb414ff6efd9b0763f
htmlcssgift <stefanigifta@gmail.com>
2025-06-26 19:48:52 +0700

    Created divisions page

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-$('#about').toggle();
-
+// navigation menu
+$('#about').hide();
 $('.bi-house-door-fill').addClass('active');
-
 $('.fa-question').on('click', function() {
-  $('#home').toggle();
-  $('#about').toggle();
+  $('#home').hide();
+  $('#about').show();
   $('.bi-house-door-fill').removeClass('active');
   $('.fa-question').addClass('active');
 });
-
 $('.bi-house-door-fill').on('click', function() {
-  $('#home').toggle();
-  $('#about').toggle();
+  $('#home').show();
+  $('#about').hide();
   $('.bi-house-door-fill').addClass('active');
   $('.fa-question').removeClass('active');
+});
+
+// divisions (about page)
+var division = ['Reporter', 'Editor', 'Layouter', 'Illustrator', 'Fotografer'];
+for(let i = 0; i < division.length; i++) {
+  let cloned = $('.division-link').first().clone();
+  cloned.find('span').text(division[i]);
+  $('.divisions').append(cloned);
+};
+$('.division-link').first().remove();
+$('.division-link').on('click', function() {
+  let divisionName = $(this).text();
+  location.href = `/divisions.html`;
 });
